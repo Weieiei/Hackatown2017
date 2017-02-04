6be6b302dc943adab2916916e6782311edbbe550
Amanda <amawai@github.com>
2017-02-04 16:28:50 -0500

    Let's get started

---
+++ src/components/Message.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <input type="text"></input>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'message',
+  data () {
+    return {
+      msg: 'mm'
+    }
+  }
+}
+</script>
