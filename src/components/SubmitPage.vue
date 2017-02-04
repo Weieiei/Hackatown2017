8d1f614514219cc988a290faa7de8b73b4c409c8
Wei He <hewei196126@gmail.com>
2017-02-04 18:34:52 -0500

    Modified the SubmitPage

--- src/components/SubmitPage.vue
+++ src/components/SubmitPage.vue
@@ -1,87 +1,46 @@
-<template>
-  <div>
-    <input type="text"></input>
+
+<template >
     <md-card>
   <md-card-media>
-    <img src="assets/card-image-1.jpg" alt="People">
+    <img id="image" src="https://evolvetours.com/wp-content/uploads/2016/11/montreal-canada-1050x6991.jpg" alt="People">
   </md-card-media>
 
   <md-card-header>
-    <div class="md-title">Title goes here</div>
-    <div class="md-subhead">Subtitle here</div>
+    <div class="md-title">Hi how are you?</div>
+
   </md-card-header>
 
-  <md-card-actions>
-    <md-button>Action</md-button>
-    <md-button>Action</md-button>
-  </md-card-actions>
+
 
   <md-card-content>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
+    <form novalidate @submit.stop.prevent="submit">
+      <md-input-container>
+        <label>First Name</label>
+        <md-input v-model="FirstName"></md-input>
+      </md-input-container>
+
+      <md-input-container>
+        <label>Email</label>
+        <md-input placeholder="Email"></md-input>
+      </md-input-container>
+
+      <md-input-container>
+        <label>How are you feeling today?</label>
+        <md-textarea></md-textarea>
+      </md-input-container>
+
+      <md-input-container>
+        <label>Because...</label>
+        <md-textarea></md-textarea>
+      </md-input-container>
+
+
   </md-card-content>
 </md-card>
 
-<md-card>
-  <md-card-header>
-    <md-card-header-text>
-      <div class="md-title">Title goes here</div>
-      <div class="md-subhead">Subtitle here</div>
-    </md-card-header-text>
 
-    <md-menu md-size="4" md-direction="bottom left">
-      <md-button class="md-icon-button" md-menu-trigger>
-        <md-icon>more_vert</md-icon>
-      </md-button>
 
-      <md-menu-content>
-        <md-menu-item>
-          <span>Call</span>
-          <md-icon>phone</md-icon>
-        </md-menu-item>
 
-        <md-menu-item>
-          <span>Send a message</span>
-          <md-icon>message</md-icon>
-        </md-menu-item>
-      </md-menu-content>
-    </md-menu>
-  </md-card-header>
-
-  <md-card-media>
-    <img src="assets/card-image-1.jpg" alt="People">
-  </md-card-media>
-
-  <md-card-content>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio. Dolores, sed accusantium quasi non, voluptas eius illo quas, saepe voluptate pariatur in deleniti minus sint. Excepturi.
-  </md-card-content>
-</md-card>
-
-<md-card>
-  <md-card-media>
-    <img src="assets/card-image-2.jpg" alt="People">
-  </md-card-media>
-
-  <md-card-content>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea nostrum.
-  </md-card-content>
-</md-card>
-
-<md-card>
-  <md-card-media>
-    <img src="assets/card-image-2.jpg" alt="People">
-  </md-card-media>
-
-  <md-card-header>
-    <div class="md-title">Title goes here</div>
-    <div class="md-subhead">Subtitle here</div>
-  </md-card-header>
-
-  <md-card-actions>
-    <md-button>Action</md-button>
-    <md-button>Action</md-button>
-  </md-card-actions>
-</md-card>
-  </div>
 </template>
 
 <script>
@@ -89,3 +48,13 @@ export default {
   name: 'SubmitPage'
 }
 </script>
+<style>
+#messageBox{
+background-image: url("https://evolvetours.com/wp-content/uploads/2016/11/montreal-canada-1050x6991.jpg");
+background-color: lightblue;
+}
+#image{
+  height: 14;
+  width: 42;
+}
+</style>
